22b9bb2e997114c60d8816ea325bf979ac2c7c5f
mohammad.badruzzaman <mohammad.badruzzaman@takeaway.com>
2019-09-26 11:35:29 +0200

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: eu-central-1_95oqicEFG'', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'eu-central-1_95oqicEFG', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '5htd9ob0mj6k0um4jlt027hs28', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-1' // e.g. us-east-2
     },
