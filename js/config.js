1273307fe8c54a59d3fadd362850ac8ee973a249
mohammad.badruzzaman <mohammad.badruzzaman@takeaway.com>
2019-09-26 11:24:25 +0200

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: eu-central-1_95oqicEFG'', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5htd9ob0mj6k0um4jlt027hs28', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
